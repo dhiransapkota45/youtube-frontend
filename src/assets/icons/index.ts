f7c3f56674c459ede28fe30fd663d873153b9589
dhiransapkota45 <dhiransapkota55@gmail.com>
2023-03-17 16:23:16 +0545

    icons added

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,17 @@
+import Hamburger from "./hamburger.svg";
+import History from "./history.svg";
+import Home from "./home.svg";
+import Library from "./library.svg";
+import Subscription from "./subscription.svg";
+import Watchlater from "./watchlater.svg";
+import YourVideos from "./yourvideos.svg";
+
+export {
+  Hamburger,
+  History,
+  Home,
+  Library,
+  Subscription,
+  Watchlater,
+  YourVideos,
+};
